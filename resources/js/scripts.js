d52e0490bb32c7763c788c327e06ce8fc5eb77bd
u0e1b <miguel.valarao@gmail.com>
2018-12-22 03:27:47 -0800

    New Style Upload

---
+++ resources/js/scripts.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var typed = new Typed(".element", {
+    // Waits 1000ms after typing "First"
+    strings: [""],
+    typeSpeed: 25,
+    backSpeed: 25,
+    backDelay: 1000,
+    startDelay: 500,
+    loop: true,
+    smartBackspace: true,
+    showCursor: false,
+    autoInsertCss: true
+  });
+  
